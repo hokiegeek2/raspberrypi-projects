f9390da3a76c261be9694a0dc57eb28cc68dbf5c
hokiegeek2 <hokiegeek2@gmail.com>
2018-07-25 22:02:32 -0400

    Cleaned up code in prep for event framework

--- src/main/python/hokiegeek2/monitoring/refrigerator.py
+++ src/main/python/hokiegeek2/monitoring/refrigerator.py
@@ -4,11 +4,10 @@ import RPi.GPIO as io
 
 class RefrigeratorAccessMonitor:
     '''
-    Sets the instance-scoped door, doorOpen, doorOpened, and doorPin attributes
+    Sets the instance-scoped door, doorOpen, doorPin, and hasBeenOpened attributes
     '''   
     def __init__(self):
-        self.doorOpened = 0
-        self.doorOpen = False
+        self.hasBeenOpened = False
         self.door=0
         self.doorPin = 20
         logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
@@ -20,21 +19,24 @@ class RefrigeratorAccessMonitor:
 
     def startMonitor(self):
         logging.info("Started Monitoring Refrigerator at %s" % datetime.now().strftime("%Y%m%d %H:%M:%S"))
-
+        
+        # Get an initialized door sensor
         doorSensor = self._getInput()
+
+        # Start a loop monitoring door open and door close events
         while True:
-            '''
-            If doorSensor.input(self.doorPin) returns true, this means the door is open 
-            '''
+            # If doorSensor.input(self.doorPin) returns true, this means the door is open 
             if doorSensor.input(self.doorPin):
-                self.doorOpened += 1
+                if self.hasBeenOpened == False:
+                    self.hasBeenOpened = True
                 logging.info("Refrigerator opened at %s" % datetime.now().strftime("%Y%m%d %H:%M:%S"))
+                # while loop that executes until the door is closed, checking every second
                 while doorSensor.input(self.doorPin):
                    time.sleep(1)
                 self.door = 0
 
             if doorSensor.input(self.doorPin) == False:
-                if (self.door == 0):
+                if self.door == 0 and self.hasBeenOpened == True:
                     logging.info("Refrigerator closed at %s" % datetime.now().strftime("%Y%m%d %H:%M:%S"))
                     self.door += 1
 
